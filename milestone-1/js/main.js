d08ec5453724f505e0e2d0a28a7dadf6901f49e4
giovannifranchi <giovanni.franchi95@gmail.com>
2023-03-17 11:30:35 +0100

    Feat: milestone 1 completed

--- milestone-1/js/main.js
+++ milestone-1/js/main.js
@@ -2,7 +2,7 @@ const {createApp} = Vue;
 const contacts =  [
     {
         name: 'Michele',
-        avatar: './img/avatar_1.png',
+        avatar: 'img/avatar_1.jpg',
         visible: true,
         messages: [
             {
@@ -24,7 +24,7 @@ const contacts =  [
     },
     {
         name: 'Fabio',
-        avatar: './img/avatar_2.png',
+        avatar: 'img/avatar_2.jpg',
         visible: true,
         messages: [
             {
@@ -46,7 +46,7 @@ const contacts =  [
     },
     {
         name: 'Samuele',
-        avatar: './img/avatar_3.png',
+        avatar: 'img/avatar_3.jpg',
         visible: true,
         messages: [
             {
@@ -68,7 +68,7 @@ const contacts =  [
     },
     {
         name: 'Alessandro B.',
-        avatar: './img/avatar_4.png',
+        avatar: 'img/avatar_4.jpg',
         visible: true,
         messages: [
             {
@@ -85,7 +85,7 @@ const contacts =  [
     },
     {
         name: 'Alessandro L.',
-        avatar: './img/avatar_5.png',
+        avatar: 'img/avatar_5.jpg',
         visible: true,
         messages: [
             {
@@ -102,7 +102,7 @@ const contacts =  [
     },
     {
         name: 'Claudia',
-        avatar: './img/avatar_5.png',
+        avatar: 'img/avatar_5.jpg',
         visible: true,
         messages: [
             {
@@ -124,7 +124,7 @@ const contacts =  [
     },
     {
         name: 'Federico',
-        avatar: './img/avatar_7.png',
+        avatar: 'img/avatar_7.jpg',
         visible: true,
         messages: [
             {
@@ -141,7 +141,7 @@ const contacts =  [
     },
     {
         name: 'Davide',
-        avatar: './img/avatar_8.png',
+        avatar: 'img/avatar_8.jpg',
         visible: true,
         messages: [
             {
